11dd2968ce11111cfe4bf4da73455d8b28dbbb63
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-01-09 20:35:55 +0200

    initial commit

--- chapter-01/example01/example01.cpp
+++ chapter-01/example01/example01.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+// Include the IOstream library for input and output.
+#include <iostream> 
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+    // The main function where the program execution begins.
+
+    std::cout << "Hello World" << std::endl; 
+    // std::cout is used to output "Hello World" to the console.
+    // << is the insertion operator, used to insert the string into the output stream.
+    // std::endl inserts a newline character and flushes the stream, ensuring the output is displayed.
+
+    return 0; 
+    // Return 0 indicates that the program has executed successfully.
 }
+
